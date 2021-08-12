4fd1f1b0399b032a81f386f3830ff0995ef9ba70
Eder <ederpvenancio@gmail.com>
2021-08-12 18:13:25 -0300

    Fix and new features

--- App.js
+++ App.js
@@ -9,20 +9,15 @@ const [altura, setAltura] = useState('');
 const [peso, setPeso] = useState('');
 const [idade, setidade] = useState('');
 const [imc, setIMC] = useState(0);
+const [classificado, setclass] = useState('');
+
+
 
 
 const CalculateIMC = () => {
   let imc = peso / (altura*altura/10000);
          setIMC(imc);
-
-      
-
-
-
- 
-  
-
-alert(`Altura: ${altura}cm \nPeso: ${peso}kg  \nSeu imc: ${imc.toFixed(2)} `);
+alert(`Altura: ${altura}cm \nPeso: ${peso}kg\n Sua idade: ${idade}\nSeu imc: ${imc.toFixed(2)} `);
 
 if (imc < 18.5 ){
  alert("Você está muito magro");
@@ -34,32 +29,81 @@ if (imc < 18.5 ){
   alert("Você está na faixa de obesidade");
 }
 
+let classificado;
 
-}
-const [result, setResult] = useState(imc);
-  return(
+  if (imc < 18.5){
+    classificado = "Magreza"
+  } else if (imc > 18.5 && imc < 25){
+    classificado = "normal"
+  } else if (imc >= 25 && imc < 30){
+    classificado = "sobrepeso"
+  } else {
+    classificado = "obesidade"
+  }
+  
 
   
+
+  setclass(classificado);
+}
+
+
+{/* bloco de comando para definir altura, peso e idade dos elementos abaixo do input */}
+const Altidpes = () => { 
+let altura = altura;
+let idade  = idade;
+let peso = peso;
+}
+
+
+{/* bloco de comando para definir o resultado do imc */}
+const [result, setResult] = useState(imc);
+
+
+  return(
   <SafeAreaView style={styles.container}>
     <StatusBar />
-
-     
-
-
-
     <Text style={styles.titulo}>CALCULE SEU IMC</Text>
 
-    {/* campo de texto altura */}
-    <TextInput 
+
+
+
+
+
+<SafeAreaView style={styles.topbar}>
+<Text style={styles.txtimc}>RESULTADO DO IMC:</Text>
+<Text style={styles.resultimc}>
+  {imc.toFixed(2)}
+</Text>
+
+<Text style={styles.txtimc}>Classificação:</Text>
+<Text style={styles.resultimc}>
+  {classificado}
+</Text>
+</SafeAreaView>
+
+
+
+
+
+<SafeAreaView style={styles.container1}>
+
+
+<div></div>
+{/* campo de texto altura */}
+<TextInput 
      style={styles.input}
     placeholder='Altura(cm)'
     keyboardType='numeric'
     value={altura}
     onChangeText={(altura)=>setAltura(altura)}
+    maxLength={3}
     />
 
-
+<Text style={(styles.contesp)}> Sua altura: </Text>
+<Text style={(styles.inputr)}>{altura}cm</Text>
 {/* campo de texto peso */}
+
 
 <TextInput 
      style={styles.input}
@@ -67,9 +111,15 @@ const [result, setResult] = useState(imc);
     keyboardType='numeric'
     value={peso}
     onChangeText={(peso)=>setPeso(peso)}
+    maxLength={3}
+
     />
+<Text style={(styles.contesp)}> Seu peso: </Text>
+<Text style={(styles.inputr)}>{peso}kg</Text>
 
     {/* campo de texto idade */}
+
+<SafeAreaView style={styles.inputs}>
 
     <TextInput 
      style={styles.input}
@@ -77,8 +127,12 @@ const [result, setResult] = useState(imc);
     keyboardType='numeric'
     value={idade}
     onChangeText={(idade)=>setidade(idade)}
+    maxLength={2}
     />
 
+    <Text style={(styles.contesp)}> Sua idade: </Text>
+    <Text style={(styles.inputr)}>{idade}anos</Text>
+    </SafeAreaView>
 {/* Button */}
 <TouchableOpacity style={styles.botao}onPress={CalculateIMC}>
 
@@ -87,13 +141,7 @@ const [result, setResult] = useState(imc);
   
 </TouchableOpacity>
 
-
-<Text style={styles.txtimc}>RESULTADO DO IMC:</Text>
-<Text style={styles.resultimc}>
-  {imc.toFixed(2)}
-</Text>
-
-
+</SafeAreaView>
 
   </SafeAreaView>
   );
@@ -108,6 +156,27 @@ const styles = StyleSheet.create({
   borderWidth: 2,
   },
 
+
+
+
+  container1:{
+    height: 320,
+    fontFamily: 'orbitron',
+    marginTop: 50,
+  },
+
+  inputr:{
+    marginLeft: 180,
+  },
+
+  contesp:{
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+    marginLeft: 150,
+    marginTop: 5,
+  },
+
   ideal:{
     fontSize:18,
     fontWeight: 'bold',
@@ -117,7 +186,6 @@ const styles = StyleSheet.create({
 
   },
   txtimc:{
-    marginLeft: 120,
     marginTop: 5,
     fontSize: 15,
     fontFamily: 'orbitron',
@@ -125,7 +193,7 @@ const styles = StyleSheet.create({
   },
 
   resultimc:{
-    marginLeft: 120,
+    marginLeft: 10,
     fontSize: 20,
   },
 
@@ -184,7 +252,32 @@ borderWidth: 1,
   resultadoimc:{
     marginLeft: 70,
     fontSize: 20,
-  }
+  },
+
+  red:{
+    backgroundColor: 'red',
+    width: '70%',
+    height: 20,
+    marginTop: 30,
+    marginLeft: 60,
+  },
+
+  green:{
+    backgroundColor: 'green',
+    width: '20%',
+    height: 20,
+
+  },
+
+  orange:{
+    backgroundColor: 'orange',
+    width: '30%',
+    height: 20,
+    marginLeft: 56,
+    marginTop: -20,
+  },
+  
+ 
 
 });
 
